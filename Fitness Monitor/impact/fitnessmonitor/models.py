76aae5ce7684d540ba62e37712bcf0d66a75fe84
m.banayosi <m.banayosi@gmail.com>
2013-04-21 19:00:28 +0200

    Added some info to the models
    Session and Round

--- Fitness Monitor/impact/fitnessmonitor/models.py
+++ Fitness Monitor/impact/fitnessmonitor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Practitioner(User):
+    
+
+class Session(models.Model):
+	trainee = models.ForeignKey(Practitioner)
+    start = models.DateTimeField(null=True)
+    end = models.DateTimeField(null=True)
+
+class Round(models.Model):
+	trainee = models.ForeignKey(Practitioner)
+    start = models.DateTimeField(null=True)
+    end = models.DateTimeField(null=True)
