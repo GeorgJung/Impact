3ae5f7674e19bf49c59ddae2045427d294ded3c9
m.banayosi <m.banayosi@gmail.com>
2013-04-22 14:28:13 +0200

    Added a test
    Page and method

--- Fitness Monitor/impact/fitnessmonitor/views.py
+++ Fitness Monitor/impact/fitnessmonitor/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.shortcuts import render_to_response, redirect, render
+from django.template import RequestContext
+
+def test(request):
+    return render_to_response("test.html", RequestContext(request))
--- Fitness Monitor/impact/impact/urls.py
+++ Fitness Monitor/impact/impact/urls.py
@@ -15,3 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('fitnessmonitor.views',
+    url(r'^test/$', "test", name="home"),
+)
