52d60e7dc1ba3c5cabff3de22e0b7984675753a8
m.banayosi <m.banayosi@gmail.com>
2013-04-22 12:18:17 +0200

    Added the identifier to the practitioner

--- Fitness Monitor/impact/fitnessmonitor/models.py
+++ Fitness Monitor/impact/fitnessmonitor/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User,UserManager
 class Practitioner(User):
     level = models.IntegerField(null=True, blank=True,default=0)
 
+    def __unicode__(self):
+        return self.first_name
+
 class Session(models.Model):
     trainee = models.ForeignKey(Practitioner)
     start = models.DateTimeField(null=True)
